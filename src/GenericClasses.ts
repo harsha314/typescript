0213641c7965e536eb85b7244868480043a6f4da
harsha314 <harshavardhan32952@gmail.com>
2023-06-29 12:10:41 +0530

    .

--- src/GenericClasses.ts
+++ src/GenericClasses.ts
@@ -1,1 +1,31 @@
-class MySet {}
+interface Backpack<Type> {
+    add: (obj: Type) => void;
+    get: () => Type;
+}
+
+interface MySet<Type> {
+    add: (x: Type) => void;
+    isPresent: (x: Type) => boolean;
+    remove: (x: Type) => void;
+}
+
+class ListSet<Type> implements MySet<Type> {
+    private nums = new Array<Type>();
+
+    add(x: Type) {
+        if (this.nums.indexOf(x) === -1) {
+            this.nums.push(x);
+        }
+    }
+
+    isPresent(x: Type) {
+        return this.nums.indexOf(x) === -1;
+    }
+
+    remove(x: Type) {
+        let index = this.nums.indexOf(x);
+        if (index !== -1) {
+            this.nums.splice(index, 1);
+        }
+    }
+}
